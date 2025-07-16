0939495538fe355628ac900ffd8eb532e8048552
Shavali388 <dudekulas54@gmail.com>
2025-07-16 12:13:45 +0530

    updated login page

--- src/com/git/practice/login/Login.java
+++ src/com/git/practice/login/Login.java
@@ -5,6 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("username");
 		System.out.println("pasword");
+		System.out.println("click on submit");
 	}
 
 }
